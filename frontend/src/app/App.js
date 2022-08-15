cae12c8436fe3af644901edee57543d0f680ef92
iamolegk1 <oleg.kropivka5@gmail.com>
2022-08-15 13:20:22 +0300

    sale page setup

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import ProductPage from "../pages/ProductPage";
+import SellPage from "../pages/SellPage";
 import { paths } from "./paths";
-const { home, fruit } = paths;
+const { home, fruit, sell } = paths;
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path={home} element={<HomePage />} />
         <Route path={`${fruit}/:id`} element={<ProductPage />} />
+        <Route path={sell} element={<SellPage />} />
       </Routes>
     </BrowserRouter>
   );
